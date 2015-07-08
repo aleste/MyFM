691ea97d81dabd611005c94af2422bf8d5a8f279
Alejandro Steinmetz <asteinmetz78@gmail.com>
2015-07-08 11:07:01 -0300

    Creada clase y layout del fragmento

---
+++ app/src/main/java/com/aleste/myfm/ui/HypedArtistsFragment.java
@@ -0,0 +1,9 @@
+package com.aleste.myfm.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Alejandro Steinmetz <asteinmetz78@gmail.com> on 07/07/15.
+ */
+public class HypedArtistsFragment extends Fragment {
+}
